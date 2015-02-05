ba03e4b402c6b839349e94bc74354a880679a36b
Hannes Walz <hannes@leuchtetgruen.de>
2015-02-05 11:33:52 +0100

    Use functions already in the element (like jquery functions) + documentation in the source

--- baseviewcontroller.js
+++ baseviewcontroller.js
@@ -9,6 +9,35 @@ var Bindable = {
 						that.set(val);
 				});
 		},
+
+		copyElementFunctions : function() {
+				var that = this;
+				var elem = this.element;
+
+				/*
+				 * Prepare list of functions to copy.
+				 * They must meet the following criteria:
+				 * - They are functions (obviously)
+				 * - They are not present in the object already (dont override)
+				 */ 
+				var fcts = [];
+				for (var key in elem) {
+						if ((typeof(elem[key]=="function")) && (typeof(this[key])=="undefined")) {
+								   fcts.push(key);
+						}
+				}
+
+				/*
+				 * Make new functions in this object and make them
+				 * call the original functions on the elem object
+				 */
+				_.each(fcts, function(fctName) {
+						that[fctName] = function() {
+								elem[fctName].apply(elem, arguments);
+						};
+				});
+
+		},
 };
 
 var Valuable = function(elem) {
@@ -29,6 +58,9 @@ var Valuable = function(elem) {
 		elem.change(function(e) {
 				f.trigger("change", elem.val());
 		});
+
+		f.element = elem;
+		f.copyElementFunctions();
 
 		return f;
 };
@@ -52,6 +84,9 @@ var HTMLable = function(elem) {
 				f.trigger("change", elem.html());
 		});
 
+		f.element = elem;
+		f.copyElementFunctions();
+
 		return f;
 };
 
@@ -59,9 +94,20 @@ var TextView = Valuable;
 var Slider = Valuable;
 var ContentView = HTMLable;
 
-
+var ViewTypes = {
+		"textview" : TextView,
+		"slider" : Slider,
+		"contentview" : ContentView
+};
 
 var BaseViewController = Backbone.View.extend({
+	/*
+	 * This is the internal hide function that should also be called
+	 * when overriding .hide()
+	 * It calls the animateHide function and afterwars hides the
+	 * element using the jQuery-hide function. If the doEmpty parameter
+	 * is set to true it also empties the container element.
+	 */
 	_hide : function(doEmpty) {
 			var that = this;
 			this.animateHide(function() {
@@ -70,6 +116,13 @@ var BaseViewController = Backbone.View.extend({
 			});
 	},
 
+	/*
+	 * This is the internal show function that should also be called
+	 * when overriding .show()
+	 * It assigns visibleViewController property to the window and
+	 * calls the jquery-show function on the element. 
+	 * Afterwars it puts the object in the foreground and calls .animateShow()
+	 */
 	_show : function() {
 			window.visibleViewController = this;
 			window.$el = this.$el;
@@ -78,6 +131,22 @@ var BaseViewController = Backbone.View.extend({
 			this.animateShow();
 	},
 
+	/*
+	 * Animation of showing the viewcontrollers element.
+	 * Only does something if the viewcontroller has a transition
+	 * property specified.
+	 * It then removes the animatable class and the visible class
+	 * that might still be there and makes the object invisible
+	 * without an animation by adding the invisible class (in the absence
+	 * of the animatable class)
+	 *
+	 * After 50ms (dont ask me why this is necessary) it adds the animatable
+	 * class again so css3-transitions will be visible and adds the
+	 * visible class.
+	 * After the animation is done it removes the invisible class and calls
+	 * a callback if provided. The callback is also called when there is
+	 * nothingto animate
+	 */
 	animateShow : function(callback) {
 			if (this.hasAnimationProperty()) {
 				visibleClass = this.getAnimationVisibleClass();
@@ -103,6 +172,16 @@ var BaseViewController = Backbone.View.extend({
 			else if (callback) callback();
 	},
 
+	/*
+	 * Animation of the hiding of the viewcontrollers element.
+	 * Only does something if the viewcontroller has a transition
+	 * property specified.
+	 * It then removes the visible css class , adds the animatable css-class (which specifies 
+	 * that transtion should take place etc.) and adds the invisible css class so the 
+	 * animation will start.
+	 * After the end of the animation or after not showing an animation the callbac will be
+	 * called if it is provided.
+	 */
 	animateHide : function(callback) {
 			if (this.hasAnimationProperty()) {
 					visibleClass = this.getAnimationVisibleClass();
@@ -125,26 +204,63 @@ var BaseViewController = Backbone.View.extend({
 			}
 	},
 
+	/*
+	 * Checks wether or not the show
+	 * and hide methods for this view
+	 * controller should use visible
+	 * css3 transitions
+	 */
 	hasAnimationProperty : function() {
 			return (this.transition);
 	},
 
+	/*
+	 * Returns the css class that should be used
+	 * for displaying this item as visible in 
+	 * the given transition
+	 */
 	getAnimationVisibleClass : function() {
 			return "visible-" + this.transition;
 	},
 
+	/*
+	 * Returns the css class that should be used
+	 * for displaying this item as invisible in 
+	 * the given transition
+	 */
 	getAnimationInvisibleClass : function() {
 			return "invisible-" + this.transition;
 	},
 
+	/*
+	 * You can override this function with your own
+	 * hide function but remember to call 
+	 * this._hide() within your code
+	 */
 	hide : function() {
 			this._hide();
 	},
 
+	/*
+	 * You can override this function with your own
+	 * show function but remember to call
+	 * this._show() within your code
+	 */
 	show : function() {
 			this._show();
 	},
 
+	/*
+	 * Render function fetches template from URL 
+	 * and fills it with given values from the values
+	 * object (using the _.template mechanism).
+	 *
+	 * It then injects the views into the viewcontroller
+	 * and calls the show function.
+	 * After being done it calls the given callback and
+	 * the doneRendering-function of the viewcontroller
+	 * (both only if provided)
+	 */
 	_render : function(url, values, callback) {
 			$.ajaxSetup({
 					// Disable caching of AJAX responses
@@ -165,8 +281,13 @@ var BaseViewController = Backbone.View.extend({
 
 	},
 
+	/*
+	 * Recursive function. Calls injectView on the root 
+	 * element and then calls itself on all its children()
+	 */
 	injectViews : function(root) {
 			this.injectView(root);
+
 			var children = $(root).children();
 			if (children.length > 0) {
 					for (var i=0; i < children.length; i++) {
@@ -176,18 +297,25 @@ var BaseViewController = Backbone.View.extend({
 			}
 	},
 
+	/*
+	 * Injects views that have certain view-type attributes into
+	 * the viewcontroller as properties
+	 */
 	injectView : function(elem) {
 			if (!elem.attr("id")) return;
 
 			var viewType = elem.attr("view-type");
-			if (viewType == "textview") {
-					this[elem.attr("id")] = TextView(elem);
-			}
-			else if (viewType == "slider") {
-					this[elem.attr("id")] = Slider(elem);
-			}
-			else if (viewType == "contentview") {
-					this[elem.attr("id")] = ContentView(elem);
+
+			/*
+			 * Use the ViewType lookup dictionary to inject
+			 * the elements into the ViewController.
+			 * This works as the views are actually constructor like functions.
+			 * and are therefore NOT to be instantiated with the new keyword.
+			 */
+			for (var vType in ViewTypes) {
+					if (viewType == vType) {
+							this[elem.attr("id")] = ViewTypes[vType].call(this, elem);
+					}
 			}
 	},
 });
